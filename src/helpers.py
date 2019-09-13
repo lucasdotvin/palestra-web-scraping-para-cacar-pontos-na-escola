c17f710e85d8eb557898af49789f883afc181fdb
iWeya <lucasvinicius211@yahoo.com>
2019-09-13 13:03:12 -0300

    Formatar código e remover identação desnecessária

--- src/helpers.py
+++ src/helpers.py
@@ -12,10 +12,10 @@ ITEM_CATEGORY = {
 
 def get_item_content_type_class(item_classes: List[str]) -> str:
     """Retorna a classe com a definição do tipo de conteúdo do item.
-    
+
     Arguments:
         item_classes {List[str]} -- Lista de classes.
-    
+
     Returns:
         str -- Classe com a definição do tipo de conteúdo do item.
     """
@@ -26,10 +26,10 @@ def get_item_content_type_class(item_classes: List[str]) -> str:
 
 def get_item_category(content_core_item) -> str:
     """Retorna a categoria adequada do item a partir de suas classes.
-    
+
     Arguments:
         content_core_item {request_html.Element} -- Objeto Element do elemento div alvo da função.
-    
+
     Returns:
         str -- Categoria do elemento. Pode ser: folder, file, link, alert ou document.
     """
@@ -47,16 +47,16 @@ def get_item_category(content_core_item) -> str:
 
 def get_item_details(
     content_core_item,
-    parent_path: str=''
+    parent_path: str = ''
 ) -> Dict[str, str]:
     """Obtém os detalhes do item fornecido.
-    
+
     Arguments:
         content_core_item {request_html.Element} -- Objeto Element do elemento div do objeto a ser detalhado.
-    
+
     Keyword Arguments:
         parent_path {str} -- Caminho do diretório pai. Quando definido, é adicionado ao caminho do objeto trabalhado pela função. (default: {''})
-    
+
     Returns:
         Dict[str, str] -- Dicionário com detalhes do item.
     """
@@ -78,16 +78,16 @@ def get_item_details(
 
 def get_page_content_items(
     page_html,
-    parent_path: str=''
+    parent_path: str = ''
 ) -> List[Dict[str, str]]:
     """Obtém os itens de uma página (diretório).
-    
+
     Arguments:
         page_html {request_html.Element} -- Objeto Element da página.
-    
+
     Keyword Arguments:
         parent_path {str} -- Caminho do diretório superior. Quando definido, é adicionado ao caminho de todos os itens filhos desse diretório. (default: {''})
-    
+
     Returns:
         List[Dict[str, str]] -- Lista de dicionários com detalhes de cada item da página.
     """
@@ -110,11 +110,11 @@ def get_page_content_items(
 
 def get_content_tree(inital_url: str, session) -> List[Dict[str, str]]:
     """Gera uma árvore de diretórios a partir da URL inicial definida.
-    
+
     Arguments:
         inital_url {str} -- URL do diretório no topo da árvore.
         session {request_html.HTMLSession} -- Objeto HTMLSession para execução das requisições HTTP.
-    
+
     Returns:
         List[Dict[str, str]] -- Lista de dicionários com detalhes dos itens e sub-itens.
     """
@@ -143,11 +143,11 @@ def compare_trees(
     """Compara duas árvores de diretórios e retorna as discrepâncias.
     Cada item da árvore-alvo será buscado na árvore-base para comparação.
     Os itens não encontrados são retornados.
-    
+
     Arguments:
         base_tree {List[Dict[str, str]]} -- Árvore base para comparação.
         target_tree {List[Dict[str, str]]} -- Árvore a ser comparada.
-    
+
     Returns:
         List[Dict[str, str]] -- Itens discrepantes entre as duas árvores.
     """

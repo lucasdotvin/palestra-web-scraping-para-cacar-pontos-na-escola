c05006b40c2b488e724fa623c61d83adf2861996
iWeya <lucasvinicius211@yahoo.com>
2019-09-13 13:02:51 -0300

    Remover typing de parâmetros de classes importadas

--- src/helpers.py
+++ src/helpers.py
@@ -24,11 +24,11 @@ def get_item_content_type_class(item_classes: List[str]) -> str:
             return class_
 
 
-def get_item_category(content_core_item: Element) -> str:
+def get_item_category(content_core_item) -> str:
     """Retorna a categoria adequada do item a partir de suas classes.
     
     Arguments:
-        content_core_item {Element} -- Objeto Element do elemento div alvo da função.
+        content_core_item {request_html.Element} -- Objeto Element do elemento div alvo da função.
     
     Returns:
         str -- Categoria do elemento. Pode ser: folder, file, link, alert ou document.
@@ -46,13 +46,13 @@ def get_item_category(content_core_item: Element) -> str:
 
 
 def get_item_details(
-    content_core_item: Element,
+    content_core_item,
     parent_path: str=''
 ) -> Dict[str, str]:
     """Obtém os detalhes do item fornecido.
     
     Arguments:
-        content_core_item {Element} -- Objeto Element do elemento div do objeto a ser detalhado.
+        content_core_item {request_html.Element} -- Objeto Element do elemento div do objeto a ser detalhado.
     
     Keyword Arguments:
         parent_path {str} -- Caminho do diretório pai. Quando definido, é adicionado ao caminho do objeto trabalhado pela função. (default: {''})
@@ -77,13 +77,13 @@ def get_item_details(
 
 
 def get_page_content_items(
-    page_html: Element,
+    page_html,
     parent_path: str=''
 ) -> List[Dict[str, str]]:
     """Obtém os itens de uma página (diretório).
     
     Arguments:
-        page_html {Element} -- Objeto Element da página.
+        page_html {request_html.Element} -- Objeto Element da página.
     
     Keyword Arguments:
         parent_path {str} -- Caminho do diretório superior. Quando definido, é adicionado ao caminho de todos os itens filhos desse diretório. (default: {''})
@@ -108,15 +108,12 @@ def get_page_content_items(
     return detailed_content_items
 
 
-def get_content_tree(
-    inital_url: str,
-    session: HTMLSession
-) -> List[Dict[str, str]]:
+def get_content_tree(inital_url: str, session) -> List[Dict[str, str]]:
     """Gera uma árvore de diretórios a partir da URL inicial definida.
     
     Arguments:
         inital_url {str} -- URL do diretório no topo da árvore.
-        session {HTMLSession} -- Objeto HTMLSession para execução das requisições HTTP.
+        session {request_html.HTMLSession} -- Objeto HTMLSession para execução das requisições HTTP.
     
     Returns:
         List[Dict[str, str]] -- Lista de dicionários com detalhes dos itens e sub-itens.
